2f12997c9bd0984d463513ec3cb692c108b09a42
Yusadolat <yusadolat@gmail.com>
2021-04-01 08:05:25 +0100

    Day 3 Solution and More questions added..

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,24 @@
 function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+  return inputArray[0];
+}
 
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+  const sumProducts: number[] = [];
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i + 1]);
+    }
+  }
+  return sumProducts;
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
