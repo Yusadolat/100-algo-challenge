be68f607da03e009d32b6a40333a7e1ec95ebe6d
Yusadolat <yusadolat@gmail.com>
2021-04-03 09:40:59 +0100

    Day 5 Problem solved...

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,41 @@
 function alphabeticShift(inputString: string): string {
-
+    const alphabet: string[] = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    let shiftedInput = inputString.split("");
+    for (let i = 0; i < inputString.length; i++) {
+      let index = 0;
+      if (shiftedInput[i] !== "z") {
+        index = alphabet.indexOf(shiftedInput[i]) + 1;
+      }
+      shiftedInput[i] = alphabet[index];
+    }
+    return shiftedInput.join("");
 }
 
 console.log(alphabeticShift('crazy'));
