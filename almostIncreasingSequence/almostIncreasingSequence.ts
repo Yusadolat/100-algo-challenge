682da8d686b168062f4a2251184c2de8a3b4bcbc
Yusadolat <yusadolat@gmail.com>
2021-04-05 07:12:11 +0100

    Dolved day 7 challenge :rocket:

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,16 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+let count = 0;
+for (let i = 0; i < sequence.length; i++){
+    if(sequence[i] <= sequence[i - 1]){
+        count++;
+    }
+    if(sequence[i] <= sequence[i + 2] && sequence[i + 1] <= sequence[i - 1]){
+        return false
 
+    }
+}
+return count <= 1
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
