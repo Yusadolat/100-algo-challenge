f717576cb1d14d6a70aad9a78d359578c093a1d6
Yusadolat <yusadolat@gmail.com>
2021-04-11 22:07:05 +0100

    formatter..

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,18 +1,17 @@
 function sumAllPrimes(num: number): number {
-let primeTotal = 0;
-for (let  i = 2; i<num; i++){
-  for(let j = 2; j<=i; j++){
-    if(i === j){
-      primeTotal += i;
+  let primeTotal = 0;
+  for (let i = 2; i < num; i++) {
+    for (let j = 2; j <= i; j++) {
+      if (i === j) {
+        primeTotal += i;
+      }
+      if (i % j === 0) {
+        break;
+      }
     }
-if(i%j === 0){
-  break;
-}
   }
-}
-return primeTotal;
+  return primeTotal;
 }
 
 console.log(sumAllPrimes(10));
 console.log(sumAllPrimes(977));
-
